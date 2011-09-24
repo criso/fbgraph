13b2614154b11635931e88fbb1f501fc52258f60
Cristiano Oliveira <ocean.cris@gmail.com>
2011-09-24 19:02:07 -0400

    graphapi.post and test cleanup - deleting test users

--- test/graph.test.js
+++ test/graph.test.js
@@ -1,204 +1,218 @@
-var graph      = require('../lib/graph')
-    , FBConfig = require('../lib/config').facebook
-    , vows     = require('vows')
-    , events   = require('events')
-    , assert   = require('assert');
+var graph    = require("../lib/graph")
+  , FBConfig = require("../lib/config").facebook
+  , vows     = require("vows")
+  , events   = require("events")
+  , assert   = require("assert");
+
+
+var testUser1      = {}
+  , appAccessToken = FBConfig.appId + "|" + FBConfig.appSecret;
 
 // Create test suite
-vows.describe('graph.test').addBatch({
-    'GraphApi access ': {
+vows.describe("graph.test").addBatch({
+  "When accessing the graphApi": {
 
-        'setting': {
-            topic: graph,
+    "setting": {
+      topic: graph,
 
-            'graphUrl should be graph.facebook.com': function (graph) {
-                assert.ok(graph.getGraphUrl() === 'graph.facebook.com');
-            }
+      "graphUrl should be graph.facebook.com": function (graph) {
+        assert.ok(graph.getGraphUrl() === "graph.facebook.com");
+      }
+    },
+
+    "with no access token": {
+      "and searching for pulblic data via username": {
+        topic: function() {
+          graph.get("/btaylor", this.callback);
         },
 
-        'tests that do *not* require an access token ': {
-            'when getting by username': {
-                topic: function() {
-                    graph.get('/btaylor', this.callback);
-                },
-
-                'should get public data': function (err, res) {
-                    assert.include(res, 'username');
-                    assert.include(res, 'name');
-                    assert.include(res, 'first_name');
-                    assert.include(res, 'last_name');
-                }
-            },
-
-            'when requesting an url for a user that does not exist': {
-                topic: function () {
-                    graph.get('/thisUserNameShouldNotExist', this.callback);
-                },
-
-                'should return an error': function (err, res) {
-                    assert.include(res, 'error');
-                }
-            },
-
-            'when using an empty api url': {
-                topic: function() {
-                    graph.get('', this.callback);
-                },
-
-                'should throw an error for parsing invalid json': function (err, res) {
-                    assert.equal(err.error, 'Error parsing json',
-                    'Should throw an error while parsing json');
-                }
-            },
-
-            'when not using a string as an api url': {
-                topic: function () {
-                    graph.get({ you: 'shall not pass' }, this.callback);
-                },
-
-                'should return an api must be a string error': function (err, res) {
-                    assert.equal(err.error, 'Graph api url must be a string',
-                    'Should return an error if api url is not a string');
-                }
-            },
-
-            'when requesting a public profile picture': {
-                topic: function () {
-                    graph.get('/zuck/picture', this.callback);
-                },
-
-                'should get an image and return a json with its location ': function (err, res) {
-                    assert.include(res, 'image');
-                    assert.include(res, 'location');
-                }
-            },
-
-            'when requesting an api url with a missing slash': {
-                topic: function () {
-                    graph.get('zuck/picture', this.callback);
-                },
-
-                'should be able to get valid data': function (err, res) {
-                    assert.include(res, 'image');
-                    assert.include(res, 'location');
-                }
-            },
-
-            'when trying to access data that requires an access token': {
-                topic: function () {
-                    graph.get('/817129783203', this.callback);
-                },
-
-                'should return an OAuthException error': function (err, res) {
-                    assert.include(res, 'error');
-                    assert.equal(res.error.type, 'OAuthException',
-                      'Response from facebook should be an OAuthException');
-                }
-            },
-
-            'when performing a public search ': {
-                topic: function () {
-                    graph.search({ q: 'watermelon', type: 'post' }, this.callback);
-                },
-
-                'should return valid data': function (err, res) {
-                    assert.ok(Array.isArray(res.data), 'response data should be an array');
-                    assert.ok(res.data.length > 1, 'response data should not be empty');
-                }
-            },
-
-        },
-
-        'tests that *require* an access token': {
-            'after getting a valid token': {
-                topic: function () {
-
-                    // create test user
-                    var testUserUrl = '/' + FBConfig.appId + '/accounts/test-users?' + 
-                        'installed=true' + 
-                        '&name=Ricky Bobby' +
-                        '&permissions=' + FBConfig.scope +
-                        '&method=post' + 
-                        '&access_token=' + FBConfig.appId + '|' + FBConfig.appSecret;
-
-                    var promise = new events.EventEmitter();
-
-                    graph.get(encodeURI(testUserUrl), function(err, res) {
-                        if (!res || res.error
-                            && res.error.message.indexOf('Service temporarily unavailable')) {
-
-                            promise.emit('error', err); 
-                            console.error("Can't retreive access token from facebook\n" +
-                                "Try again in a few minutes");
-                        } else {
-
-                            graph.setAccessToken(res.access_token);
-                            promise.emit('success', res);
-                        }
-                    });
-
-                    return promise;
-                },
-
-                // following tests will only happen after 
-                // an access token has been set
-                'should have valid keys': function(err, res) {
-                    assert.isNull(err);
-                    assert.include(res, 'id');
-                    assert.include(res, 'access_token');
-                    assert.include(res, 'login_url');
-                    assert.include(res, 'email');
-                    assert.include(res, 'password');
-                },
-
-                'when getting data from a protected page': {
-                    topic: function () {
-                        graph.get('/817129783203', this.callback);
-                    },
-
-                    'response should be valid': function(err, res) {
-                        assert.isNull(err);
-                        assert.equal('817129783203', res.id, 'response id should be valid');
-                    }
-                },
-
-                'when getting a user permissions': {
-                    topic: function () {
-                        graph.get('/me/permissions', this.callback);
-                    },
-
-                    'test user should have proper permissions': function (err, res) {
-                        assert.isNull(err);
-
-                        var permissions = FBConfig.scope
-                                            .replace(/ /g,'')
-                                            .split(',');
-
-                        permissions.push('installed');
-                        permissions.forEach(function(key) {
-                            assert.include(res.data[0], key);
-                        });
-                    }
-                },
-
-                'when performing a search': {
-                    topic: function () {
-                        var searchOptions = {
-                              q:       'coffee'
-                            , type:    'place'
-                            , center:  '37.76,-122.427'
-                            , distance: 1000
-                        };
-
-                        graph.search(searchOptions, this.callback);
-                    },
-
-                    'an access token required search should return valid data': function (err, res) {
-                        assert.isNull(err);
-                        assert.ok(res.data.length > 1, 'response data should not be empty');
-                    }
-                }
-            }
+        "should get public data": function (err, res) {
+          assert.include(res, "username");
+          assert.include(res, "name");
+          assert.include(res, "first_name");
+          assert.include(res, "last_name");
         }
+      },
+
+      "and requesting an url for a user that does not exist": {
+        topic: function () {
+          graph.get("/thisUserNameShouldNotExist", this.callback);
+        },
+
+        "should return an error": function (err, res) {
+          assert.include(res, "error");
+        }
+      },
+
+      "and using an empty api url": {
+        topic: function() {
+          graph.get("", this.callback);
+        },
+
+        "should throw an error for parsing invalid json": function (err, res) {
+          assert.equal(err.error, "Error parsing json",
+            "Should throw an error while parsing json");
+        }
+      },
+
+      "and not using a string as an api url": {
+        topic: function () {
+          graph.get({ you: "shall not pass" }, this.callback);
+        },
+
+        "should return an api must be a string error": function (err, res) {
+          assert.equal(err.error, "Graph api url must be a string",
+          "Should return an error if api url is not a string");
+        }
+      },
+
+      "and requesting a public profile picture": {
+        topic: function () {
+          graph.get("/zuck/picture", this.callback);
+        },
+
+        "should get an image and return a json with its location ": function (err, res) {
+          assert.include(res, "image");
+          assert.include(res, "location");
+        }
+      },
+
+      "and requesting an api url with a missing slash": {
+        topic: function () {
+          graph.get("zuck/picture", this.callback);
+        },
+
+        "should be able to get valid data": function (err, res) {
+          assert.include(res, "image");
+          assert.include(res, "location");
+        }
+      },
+
+      "and trying to access data that requires an access token": {
+        topic: function () {
+          graph.get("/817129783203", this.callback);
+        },
+
+        "should return an OAuthException error": function (err, res) {
+          assert.include(res, "error");
+          assert.equal(res.error.type, "OAuthException",
+            "Response from facebook should be an OAuthException");
+        }
+      },
+
+      "and performing a public search ": {
+        topic: function () {
+          graph.search({ q: "watermelon", type: "post" }, this.callback);
+        },
+
+        "should return valid data": function (err, res) {
+          assert.ok(Array.isArray(res.data), "response data should be an array");
+          assert.ok(res.data.length > 1, "response data should not be empty");
+        }
+      },
+
+    },
+
+    "with an `Access Token`": {
+      topic: function () {
+
+        // create test user
+        var testUserUrl = "/" + FBConfig.appId + "/accounts/test-users?" + 
+          "installed=true" + 
+          "&name=Ricky Bobby" +
+          "&permissions=" + FBConfig.scope +
+          "&method=post" + 
+          "&access_token=" + appAccessToken;
+
+        var promise = new events.EventEmitter();
+
+        graph.get(encodeURI(testUserUrl), function(err, res) {
+          if (!res || res.error
+          && res.error.message.indexOf("Service temporarily unavailable")) {
+
+            promise.emit("error", err);
+            console.error("Can't retreive access token from facebook\n" +
+            "Try again in a few minutes");
+          } else {
+
+            graph.setAccessToken(res.access_token);
+            testUser1 = res;
+            promise.emit("success", res);
+          }
+        });
+
+        return promise;
+      },
+
+      // following tests will only happen after 
+      // an access token has been set
+      "result `keys` should be valid": function(err, res) {
+        assert.isNull(err);
+        assert.include(res, "id");
+        assert.include(res, "access_token");
+        assert.include(res, "login_url");
+        assert.include(res, "email");
+        assert.include(res, "password");
+      },
+
+      "and getting data from a protected page": {
+        topic: function () {
+          graph.get("/817129783203", this.callback);
+        },
+
+        "response should be valid": function(err, res) {
+          assert.isNull(err);
+          assert.equal("817129783203", res.id, "response id should be valid");
+        }
+      },
+
+      "and getting a user permissions": {
+        topic: function () {
+          graph.get("/me/permissions", this.callback);
+        },
+
+        "test user should have proper permissions": function (err, res) {
+          assert.isNull(err);
+
+          var permissions = FBConfig.scope
+            .replace(/ /g,"")
+            .split(",");
+
+          permissions.push("installed");
+          permissions.forEach(function(key) {
+            assert.include(res.data[0], key);
+          });
+        }
+      },
+
+      "and performing a search": {
+        topic: function () {
+          var searchOptions = {
+              q:       "coffee"
+            , type:    "place"
+            , center:  "37.76,-122.427"
+            , distance: 1000
+          };
+
+          graph.search(searchOptions, this.callback);
+        },
+
+        "an access token required search should return valid data": function (err, res) {
+          assert.isNull(err);
+          assert.ok(res.data.length > 1, "response data should not be empty");
+        }
+      }
     }
+  }
+}).addBatch({
+  "When tests are over": {
+    topic: function () {
+      var deleteUrl = testUser1.id + "?method=delete";
+      graph.post(deleteUrl, this.callback);
+    },
+
+    "test users should be removed": function(res){
+      assert.isTrue(res);
+    }
+  }
 }).export(module);
--- test/user.test.js
+++ test/user.test.js
@@ -1,79 +1,94 @@
-var graph      = require('../lib/graph')
-    , FBConfig = require('../lib/config').facebook
-    , vows     = require('vows')
-    , events   = require('events')
-    , assert   = require('assert');
+var graph    = require("../lib/graph")
+  , FBConfig = require("../lib/config").facebook
+  , vows     = require("vows")
+  , events   = require("events")
+  , assert   = require("assert");
 
 
-var testUser1   = {}
-    , testUser2 = {};
+var testUser1      = {}
+  , testUser2      = {}
+  , appAccessToken = FBConfig.appId + "|" + FBConfig.appSecret;
 
 // Create test suite
-vows.describe('user').addBatch({
-    'POST ': {
-        topic: function () {
-            var testUserUrl = '/' + FBConfig.appId + '/accounts/test-users?' + 
-                'installed=true' + 
-                '&name=Ricky Bobby' +
-                '&permissions=' + FBConfig.scope +
-                '&method=post' +
-                '&access_token=' + FBConfig.appId + '|' + FBConfig.appSecret;
+vows.describe("user").addBatch({
+  "POST ": {
+    topic: function () {
+      var testUserUrl = "/" + FBConfig.appId + "/accounts/test-users?" + 
+        "installed=true" + 
+        "&name=El Diablo" +
+        "&permissions=" + FBConfig.scope +
+        "&method=post" +
+        "&access_token=" + appAccessToken;
 
-            graph.get(encodeURI(testUserUrl), this.callback);
+      graph.get(encodeURI(testUserUrl), this.callback);
+    },
+
+    "after creating `User1` ": {
+      topic: function (res) {
+        testUser1 = res;
+
+        var testUserUrl = "/" + FBConfig.appId + "/accounts/test-users?" + 
+          "installed=true" +
+          "&name=magic man" +
+          "&permissions=" + FBConfig.scope +
+          "&method=post" + 
+          "&access_token=" + appAccessToken;
+
+        graph.get(encodeURI(testUserUrl), this.callback);
+      },
+
+      "after creating `User2` ": {
+        topic: function (res) {
+          testUser2 = res;
+
+          // The first call should be made with access token of user1
+          // This will creates a friend request from user1 to user2
+          var apiUrl =  testUser1.id + "/friends/" + testUser2.id + "?method=post";
+
+          graph.setAccessToken(testUser1.access_token);
+          graph.get(encodeURI(apiUrl), this.callback);
         },
 
-        'after creating `User1` ': {
-            topic: function (res) {
-                testUser1 = res;
+        "when sending a friend request from `User1` to `User2` ": {
 
-                var testUserUrl = '/' + FBConfig.appId + '/accounts/test-users?' + 
-                    'installed=true' + 
-                    '&name=magic man' +
-                    '&permissions=' + FBConfig.scope +
-                    '&method=post' + 
-                    '&access_token=' + FBConfig.appId + '|' + FBConfig.appSecret;
+          topic: function (res) {
+            var apiUrl =  testUser2.id + "/friends/" + testUser1.id + "?method=post";
 
-                graph.get(encodeURI(testUserUrl), this.callback);
-            },
+            // The second call should be made with access
+            // token for user2 and will confirm the request.
+            graph.setAccessToken(testUser2.access_token);
+            graph.get(encodeURI(apiUrl), this.callback);
+          },
 
-            'after creating `User2` ': {
-                topic: function (res) {
-                    testUser2 = res;
-
-                    // The first call should be made with access token of user1
-                    // This will creates a friend request from user1 to user2
-                    var apiUrl =  testUser1.id + '/friends/' + testUser2.id + '?method=post';
-
-                    graph.setAccessToken(testUser1.access_token);
-                    graph.get(encodeURI(apiUrl), this.callback);
-                },
-
-                'when sending a friend request from `User1` to `User2` ': {
-
-                    topic: function (res) {
-                        var apiUrl =  testUser2.id + '/friends/' + testUser1.id + '?method=post';
-
-                        // The second call should be made with access
-                        // token for user2 and will confirm the request.
-                        graph.setAccessToken(testUser2.access_token);
-                        graph.get(encodeURI(apiUrl), this.callback);
-                    },
-
-                    '`User2` should accept friend request from `User1` ': function (res) {
-                        assert.isTrue(res);
-                    }
-                }
-            }
-        },
-
-        // following tests will only happen after 
-        // an access token has been set
-        'should have valid keys': function(err, res) {
-            assert.include(res, 'id');
-            assert.include(res, 'access_token');
-            assert.include(res, 'login_url');
-            assert.include(res, 'email');
-            assert.include(res, 'password');
+          "`User2` should accept friend request from `User1` ": function (res) {
+            assert.isTrue(res);
+          }
         }
+      }
     }
+  }
+}).addBatch({
+  "When tests are over": {
+    "test *user 1*": {
+      topic: function () {
+        var deleteUrl = testUser1.id + "?method=delete";
+        graph.post(deleteUrl, {access_token: appAccessToken}, this.callback);
+      },
+
+      "should be removed": function(res){
+        assert.isTrue(res);
+      }
+    },
+
+    "test *user 2*": {
+      topic: function () {
+        var deleteUrl = testUser2.id + "?method=delete";
+        graph.post(deleteUrl, {access_token: appAccessToken}, this.callback);
+      },
+
+      "should be removed": function(res){
+        assert.isTrue(res);
+      }
+    }
+  }
 }).export(module);

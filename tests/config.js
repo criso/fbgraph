4d5a48cfa086635494654e8c3a5931dd7d4d4626
Cristiano Oliveira <ocean.cris@gmail.com>
2011-11-05 08:44:27 -0400

    removing test config data

--- tests/config.js
+++ tests/config.js
@@ -1,8 +1,8 @@
 // Facebook app config for tests
 module.exports = {
   facebook: {
-    appId:      '193097990710217',
-    appSecret:  '1242bb60970bff12916baca437cb0492',
+    appId:      'YOUR APP ID',
+    appSecret:  'YOUR APP SECRET',
     scope:      'email, user_about_me, user_birthday, user_location, publish_stream, read_stream, friends_location',
     callback:   'http://localhost:3000/'
   }
